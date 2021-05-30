ef76cca7b093f75a40770e969374d7259aa7958b
Atharva Vijay Khade <atharvavk123@gmail.com>
2021-05-30 22:53:43 +0530

    Notes Added

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -6,7 +6,7 @@ import 'home_viewModel.dart';
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ViewModelBuilder<HomeViewModel>.reactive(
+    return ViewModelBuilder<HomeViewModel>.nonReactive(
       builder: (context, model, child) => Scaffold(
         body: Center(
           child: Text("${model.title} : ${model.cnt}"),
